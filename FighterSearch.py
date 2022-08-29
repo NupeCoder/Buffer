be2bb68915760ae1443f3b587018f5ec192700fe
Abubakar Liman <limanzaffar@gmail.com>
2022-08-29 18:10:54 +0100

    Add files via upload

--- FighterSearch.py
+++ FighterSearch.py
@@ -1,24 +1,31 @@
 import requests as r
 import re
-class Search:
-    def __init__(self):
-        self.name = ''
         
-
-    def Name(self, prename):
+spacex = '\W*.\W*.*'
+def Name(prename):
         
-        self.name = ''
-        for i in prename:
-            if i == "'":
-                i = "%27"
-            self.name += i
+    name = ''
+    for i in prename:
+        if i == "'":
+            i = "%27"
+        name += i
         
-        return self.name
-    
-    def Search(self, name):
-        page = r.get('http://ufcstats.com/statistics/fighters/search?query=' + self.Name(name))
-        search = re.findall('http://ufcstats.com/fighter-details/.{16}', page.text)
-        search = search[0]
-        fighterpage = r.get(search)
+    return name
+
+def splitter(var):
+    var = var[0]
+    var = var.split("\n      ")
+    var = var[0] + var[-1]
+    return var
+
+def search(fighter):
+    page = r.get('http://ufcstats.com/statistics/fighters/search?query=' + Name(fighter))
+    search = re.findall('http://ufcstats.com/fighter-details/.{16}', page.text)
+    search = search[0]
+    fighterpage = r.get(search)
+    height = re.findall('Height:'+ spacex + '"', fighterpage.text)
+    splitter(height)
+    print (height)
 
 
+search('Kamaru')
